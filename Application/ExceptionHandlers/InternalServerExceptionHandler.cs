41cca7e531d6f6fcc42c086bba4ca7915149a628
Nikita Pohutsa <npohutsa@gmail.com>
2024-02-28 16:49:53 +0100

    ExceptionHandlers added

---
+++ Application/ExceptionHandlers/InternalServerExceptionHandler.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.ExceptionHandlers;
+
+public class InternalServerExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        if (exception is not BadHttpRequestException)
+            return false;
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        await httpContext.Response.WriteAsJsonAsync(new ProblemDetails()
+        {
+            Status = httpContext.Response.StatusCode,
+            Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1",
+            Detail = exception.Message
+        }, cancellationToken);
+        return true;
+    }
+}
